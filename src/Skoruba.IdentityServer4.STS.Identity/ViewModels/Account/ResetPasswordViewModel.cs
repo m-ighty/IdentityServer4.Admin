922f1f90c1b65d39df69523cf8579ebe4fe56aad
Jens Yvan De Craecker <decraje@cronos.be>
2021-12-20 11:57:09 +0100

    Reset form in linde brand

--- src/Skoruba.IdentityServer4.STS.Identity/ViewModels/Account/ResetPasswordViewModel.cs
+++ src/Skoruba.IdentityServer4.STS.Identity/ViewModels/Account/ResetPasswordViewModel.cs
@@ -6,14 +6,17 @@ namespace Skoruba.IdentityServer4.STS.Identity.ViewModels.Account
     {
         [Required]
         [EmailAddress]
+        [Display(Name = "Email")]
         public string Email { get; set; }
 
         [Required]
         [DataType(DataType.Password)]
+        [Display(Name = "Password")]
         public string Password { get; set; }
 
         [DataType(DataType.Password)]
         [Compare("Password")]
+        [Display(Name = "ConfirmPassword")]
         public string ConfirmPassword { get; set; }
 
         public string Code { get; set; }
